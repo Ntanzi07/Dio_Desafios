9c32cc010e1cc3cfbd2f8c38a2ff761285aff871
Nathan Tanzi <nathan.tanzi12345@gmail.com>
2024-10-08 15:13:32 -0300

    atualização dos comentarios em Classes_de_um_Jogo

--- Classes_de_um_Jogo/index.js
+++ Classes_de_um_Jogo/index.js
@@ -1,17 +1,17 @@
 class heroi{
     constructor(nome, idade, tipo){
-        // cira as variaveis do objeto
+        // Criando as variaveis do objeto
         this.nome = nome;
         this.idade = idade;
         this.tipo = tipo;
     }
 
-    // método atacar
+    // Método atacar
     atacar(){
-        // variavel para definir o tipo do ataque
+        // Variavel para definir o tipo do ataque
         let tipoAtaque;
 
-        // 
+        // Verificando o tipo do ataque referente ao tipo do heroi!
         switch(this.tipo){
             case "mago":
                 tipoAtaque = "magia" // se mago -> no ataque exibir (usou magia)
@@ -28,20 +28,23 @@ class heroi{
             default:
                 tipoAtaque = "mão" // valor caso o tipo não esteje entre os selecionados
         }
-
+        // Atacando!
         console.log(`O ${this.tipo} ${this.nome} atacou usando ${tipoAtaque}`);
-
     }
 
+    
 }
 
+// função main
 function main(){
 
+    //instanciando os novos heróis!
     let mago = new heroi("Zephyr", 113, "mago")
     let guerreiro = new heroi("Phineas", 49, "guerreiro")
     let monje = new heroi("Xander", 80, "monje")
     let ninja = new heroi("Rhys", 20, "ninja")
 
+    //usando o ataque!
     mago.atacar();
     guerreiro.atacar();
     monje.atacar();
